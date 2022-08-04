f94b9c1b39b6d297206689ffede295f0593a364f
George Baker <110601265+notgeorgebaker@users.noreply.github.com>
2022-08-04 11:40:17 +0100

    Add files via upload

---
+++ logintohpc.sh
@@ -0,0 +1,8 @@
+ssh notgeorgebaker@login1.hpc.icg.port.ac.uk 
+
+
+
+
+#Change the notgeorgebaker to your username given by the moderators of your high performance compute cluster/ supercomputer. Make sure you use the correct suffix after the @ too. 
+
+# The shell script follows this pattern.     ssh who-you-are@where-you're-going 
